ef90789c8da0e31b512d669dc2f9ea2fabd2274b
manoj <manoj@MANOJH-MT>
2022-12-10 14:58:29 +0530

    Add project files.

---
+++ LoginAPI/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using LoginAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+        [Route("api/values/GetLogin")]
+        [HttpPost]
+        public string GetLogin(Login login)
+        {
+            BAL bal = new BAL();
+            String response = bal.GetLogin(login);
+            return response;
+        }
+    }
+}
---
+++ LoginAPI/Scripts/PageScript/index.js
@@ -0,0 +1,23 @@
+﻿
+var GetLogin = function () {
+    var Username = $('#txtUsername').val();
+    var Password = $('#txtPassword').val();
+
+    var loginurl = "/api/values/GetLogin";
+
+    var logindata = JSON.stringfy({ "Username": username, "Password": password });
+
+    $.ajax({
+        type: "POST",
+        data: logindata,
+        url: loginurl,
+        contentType: "application/json;charset=uft-8",
+        dataType: "json",
+        success: function (result) {
+            alert(result)
+        },
+        error: function (result) {
+            alert(result)
+        },
+    });
+};
